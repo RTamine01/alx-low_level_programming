544ba12c69ca9422912df335f9eaf44c30526666
RTamine01 <amne0002@gmail.com>
2023-09-08 06:59:37 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main -Entry point
  *
  * description: C program that prints with put function
  *
- * Return Always 0 (success)
- * */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts()"\"Programming is like building a multilingual puzzle";
-	return(0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
