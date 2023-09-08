d84939c9fa51822ad8708845865a18d1915534a3
RTamine01 <amne0002@gmail.com>
2023-09-07 18:06:28 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a C program that print with printf funtion
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}	
+
