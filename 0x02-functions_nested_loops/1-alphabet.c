983d77589cb46779317ccabd5a8da83e5ec30dd1
RTamine01 <amne0002@gmail.com>
2023-09-12 11:45:21 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * entry - point
+ * print alphabt in lower case
+ * Return: always 0 (success0)
+ */
+
+void print_alphabet(void);
+{
+	char letter'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+		}
