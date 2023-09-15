b773591bf4dccd527ebcfd4ee95a9175b8045255
RTamine01 <amne0002@gmail.com>
2023-09-15 11:02:40 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	char c;
-	for (c >= 48; c <= 57; c++)
-	{
-		_putchar(c);
-	}
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
-
