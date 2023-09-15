ac27ab507c4c7804261e4b8b95587748bb0f33f2
RTamine01 <amne0002@gmail.com>
2023-09-15 11:35:55 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,8 @@
 
 /**
  *  * more_numbers - print more numbers
+ *  only use _putchar twice
+ *  Return: always 0 (success)
  */
 
 void more_numbers(void)
@@ -9,7 +11,7 @@ void more_numbers(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
-	{
+i	{
 		if (num == 2 || num == 4)
 			continue;
 		_putchar(num + 48);
