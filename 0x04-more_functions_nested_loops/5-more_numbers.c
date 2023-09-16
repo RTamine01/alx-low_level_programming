fcaa51b14f8f4bc5513607c680167b0ec283fbd7
RTamine01 <amne0002@gmail.com>
2023-09-15 19:16:44 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int num, row, count;
 
-	for (i = 0; i < 10; i++)
+	for (row = 1; row <= 10; row++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (count = 0; count <= 14; count++)
 		{
-			(j > 9) ? _putchar((j / 10) + '0') : 0;
-			_putchar((j % 10) + '0');
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar('\n');
 		}
-		_putcharputchar('\n');
 	}
 }
