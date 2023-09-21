04fed69e857b76834048155f10bd9022ee8cbe59
RTamine01 <amne0002@gmail.com>
2023-09-21 16:25:07 -0700

    more pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,9 @@ char *string_toupper(char *n)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if (str[i] >= 97 && str[i] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-				str[i] = str[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);
