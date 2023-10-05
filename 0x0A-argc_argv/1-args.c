17b320ed17dd38ee29f52a8a89a162788cda9124
RTamine01 <amne0002@gmail.com>
2023-10-05 12:19:45 -0700

    argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,10 @@
 /**
  *  * main - print number of arguments passed.
  *   * @argc: number of command line arguments.
- *    * @argv: pointer to array of command line arguments.
- *     *
+ *    * @argv: pointer to array of command line arguments.i
  *      * Return: Always 0 (Success)
  */
-int main(int argc, char argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
