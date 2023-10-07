35732d9f5c167cf82934cce7283fdb9f735e175d
RTamine01 <amne0002@gmail.com>
2023-10-06 17:25:24 -0700

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,9 +31,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
+
 	for (j = 0; j < n; j++)
 	{
-		str[i] = s2[i];
+		str[i] = s2[j];
 		i++;
 	}
 
