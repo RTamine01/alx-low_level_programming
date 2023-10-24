2ea1a15c8ca08e79ec690d5580f1e1640657fc33
RTamine01 <amne0002@gmail.com>
2023-10-24 07:59:57 -0700

    more linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,33 +11,70 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = NULL;
+	listint_t *previous_node = NULL;
 	unsigned int i = 0;
 
-	if (!head || !new_node)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || idx > listint_len(*head))
+	{
+		free(new_node);
 		return (NULL);
-
+	}
 	new_node->n = n;
 	new_node->next = NULL;
-	if (idx)
+	while (head != NULL)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-	node = *head;
-	while (node)
-	{
-		if (i == idx - 1)
+		if (i == idx)
 		{
-			new_node->next = node->next;
-			node->next = new_node;
+			if (i == 0)
+			{
+				new_node->next = *head;
+				*head = new_node;
+				return (new_node);
+			}
+			new_node->next = previous_node->next;
+			previous_node->next = new_node;
 			return (new_node);
 		}
+		else if ((i + 1) == idx)
+			previous_node = *head;
+		head = &((*head)->next);
 		i++;
-		node = node->next;
 	}
-	free(new_node);
 	return (NULL);
 }
+
+/**
+ *  * listint_len - counts the number of nodes in a linked list
+ *   * @h: head of the list
+ *    *
+ *     * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
